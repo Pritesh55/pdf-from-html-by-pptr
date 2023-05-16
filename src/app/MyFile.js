84d5aa52a2edfda7c2b8cc3679e2b6accc8e013c
Kinnari-Shah <iampritesh13@gmail.com>
2023-05-16 19:06:52 +0530

    Update MyFile.js

--- src/app/MyFile.js
+++ src/app/MyFile.js
@@ -54,12 +54,13 @@ const MyFile = () => {
                                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
                                     <div class="h-1 w-20 bg-indigo-500 rounded"></div>
                                 </div>
-                                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+                                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably have not heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
                             </div>
                             <div class="flex flex-wrap -m-4">
                                 <div class="xl:w-1/4 md:w-1/2 p-4">
                                     <div class="bg-gray-100 p-6 rounded-lg">
-                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/river-219972_640.jpg" alt="content" width={724} height={404} ></Image>
+                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/river-219972_640.jpg" alt="content" width={724} height={404} />
+                                        
                                             <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
                                             <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
                                             <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -67,7 +68,7 @@ const MyFile = () => {
                                 </div>
                                 <div class="xl:w-1/4 md:w-1/2 p-4">
                                     <div class="bg-gray-100 p-6 rounded-lg">
-                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/beach-394503_640.jpg" alt="content" width={724} height={404} ></Image>
+                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/beach-394503_640.jpg" alt="content" width={724} height={404} />
                                             <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
                                             <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
                                             <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -75,7 +76,7 @@ const MyFile = () => {
                                 </div>
                                 <div class="xl:w-1/4 md:w-1/2 p-4">
                                     <div class="bg-gray-100 p-6 rounded-lg">
-                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/butterflies-1127666_640.jpg"  alt="content" width={724} height={404}></Image>
+                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/butterflies-1127666_640.jpg"  alt="content" width={724} height={404} />
                                             <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
                                             <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
                                             <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
@@ -83,7 +84,7 @@ const MyFile = () => {
                                 </div>
                                 <div class="xl:w-1/4 md:w-1/2 p-4">
                                     <div class="bg-gray-100 p-6 rounded-lg">
-                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/tree-832079_640.jpg" width={724} height={404} alt="content"></Image>
+                                        <Image class="h-40 rounded w-full object-cover object-center mb-6" src="/tree-832079_640.jpg" width={724} height={404} alt="content" />
                                             <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
                                             <h2 class="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
                                             <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
