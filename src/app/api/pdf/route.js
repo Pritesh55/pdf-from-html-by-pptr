f1a5c3705b5288a9bcc4e350e09be6ba9cca680b
Kinnari-Shah <iampritesh13@gmail.com>
2023-05-18 18:33:12 +0530

    Update route.js

--- src/app/api/pdf/route.js
+++ src/app/api/pdf/route.js
@@ -5,7 +5,7 @@ export async function GET(request) {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
 
-  await page.goto('http://localhost:3000')
+  await page.goto('https://www.github.com')
   await page.emulateMediaType('screen');
 
   const pdfBuffer = await page.pdf({
