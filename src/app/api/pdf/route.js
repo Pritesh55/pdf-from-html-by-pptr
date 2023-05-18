7b4cf53d28c8cb66899883b395d64960f986e5da
Kinnari-Shah <iampritesh13@gmail.com>
2023-05-18 18:35:54 +0530

    Update route.js

--- src/app/api/pdf/route.js
+++ src/app/api/pdf/route.js
@@ -5,12 +5,12 @@ export async function GET(request) {
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
 
-  await page.goto('https://www.github.com')
+  await page.goto('https://pdf-from-html-by-pptr.vercel.app/')
   await page.emulateMediaType('screen');
 
   const pdfBuffer = await page.pdf({
     format: 'A4', landscape: true, printBackground: true,
-    margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+    margin: { top: '20px', right: '50px', bottom: '20px', left: '50px' },
   })
 
   await browser.close();
